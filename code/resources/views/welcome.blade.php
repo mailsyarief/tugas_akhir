a291f787bd038548c147cfc9e95a068aef0b8955
mailsyarief <ismail.syarief16@mhs.if.its.ac.id>
2020-01-15 23:52:12 +0700

    front

--- code/resources/views/welcome.blade.php
+++ code/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    TEST TEST
+                    TEST TEST ealaaaaaa
                 </div>
 
                 <div class="links">
